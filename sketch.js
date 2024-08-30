d6db0d27122de5a138b452693f787ce0a0f66406
Chris228222 <00001129778204sp@al.educacao.sp.gov.br>
2024-08-30 12:26:33 -0300

    Update and rename README.md to sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  (600, 600);
+   background("White")
+
+
+function draw() {
+  
+  stroke ("blue");
+  fill ("red");
+ 
+  
+  // console.log(mouseIsPressed);
+
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
